068a7590e4ebf79ec4b4da5820c75a36f54e59c8
LikemDzokoto <likemdzokoto@gmail.com>
2024-12-18 11:35:05 +0000

    create fluid interface

--- contracts/interfaces/IFluid.sol
+++ contracts/interfaces/IFluid.sol
@@ -1,7 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
+
+import {Struct} from "../libraries/Struct.sol";
+
 interface IFluid{
     
+    function createStream() external payable;
+
+
+    function withdrawFromStream() external;
+
+
+    function closeStream() external;
+
+
+
+    function pauseStream() external;
+
+
+    function resumeStream() external;
+
+
+
+    function getStream(uint256 streamId)external view returns (Struct.Stream memory);
+
+    function feeRecipient()  external view returns (address);
+
+
 
 }
---
+++ contracts/libraries/Struct.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+
+
+library Struct{
+
+
+ struct Stream{
+
+    address sender;
+    address recipient;
+    uint256 deposit;
+    uint256 startTime;
+    uint256 stopTime;
+    address tokenAddress;
+    uint256 interval;
+    uint256 createAt;
+
+ }
+
+ struct CreateStreamParams{
+   address recipient;
+   address sender;
+   uint256 deposit;
+   address tokenAdddress;
+   uint256 startTime;
+   uint256 stopTime;
+   uint256 interval;
+   uint256 cliffTime;
+   uint256 cliffAmount;
+
+}
+
+
+}
